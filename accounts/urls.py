a529caaac919b50049736c48f3e6eef9cf513ad3
Boby <boqn_bonev@abv.bg>
2018-11-21 22:15:46 +0200

    Some improvements

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('signup', views.signup, name='signup'),
- 	path('login', views.login, name='login'),
- 	path('logout', views.logout, name='logout'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
